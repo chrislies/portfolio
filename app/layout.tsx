c55586c05411a0c641f25fd49a8aec8061c4e772
root <root@DESKTOP-UQ99FV2>
2024-01-04 19:22:28 -0500

    updated body stlying

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-[url('/images/bg.svg')] bg-cover bg-no-repeat">
         <Navbar />
         <main className="">{children}</main>
         <Footer />

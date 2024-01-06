09609906a15bc588cf5495ca402ac53b648f8711
root <root@DESKTOP-UQ99FV2>
2024-01-06 00:52:15 -0500

    removed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Calibre } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "@/components/Footer";
